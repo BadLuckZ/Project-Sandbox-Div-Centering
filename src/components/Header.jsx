0f5496b4308e9997933fcb0073569d9662fc5059
BadLuckZ <thanagorn8802@gmail.com>
2024-10-30 23:52:13 +0700

    Minifix Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,12 +19,11 @@ import { categoryData, handleScrollToTop } from "../js/utils";
 import { useNavigate } from "react-router-dom";
 import { CartContext } from "../contexts/CartContext";
 import { CategoryContext } from "../contexts/CategoryContext";
-import SidebarSelector from "./SidebarSelector";
 
 const Header = ({ currentPermalink = null }) => {
   const { activeCategory, setActiveCategory } = useContext(CategoryContext);
-  const navigate = useNavigate();
   const { cart } = useContext(CartContext);
+  const navigate = useNavigate();
   const isSmallScreen = useMediaQuery("(max-width:768px)");
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
@@ -38,10 +37,7 @@ const Header = ({ currentPermalink = null }) => {
     setSidebarOpen(false);
   };
 
-  const categoryTypes = [
-    "Home",
-    ...new Set(categoryData.map((item) => item.type)),
-  ];
+  const categoryTypes = [...new Set(categoryData.map((item) => item.type))];
   const totalItemsInCart = cart.reduce(
     (count, item) => count + item.quantity,
     0
@@ -51,47 +47,50 @@ const Header = ({ currentPermalink = null }) => {
     <>
       <AppBar
         position="static"
-        sx={{
-          display: "flex",
-          backgroundColor: "var(--Project-Sandbox-Black)",
-        }}
+        sx={{ backgroundColor: "var(--Project-Sandbox-Black)" }}
       >
         <Container maxWidth="xl">
           <Toolbar
             disableGutters
             sx={{
               minHeight: "56px !important",
-              gap: "10.417px",
+              gap: "10px",
               position: "relative",
             }}
           >
             {isSmallScreen && (
-              <i
-                className="fa-solid fa-bars"
+              <IconButton
                 onClick={() => setSidebarOpen(true)}
-                style={{
-                  display: "flex",
-                  width: "40px",
-                  height: "40px",
-                  padding: "12px 8.373px 12px 9px",
-                  justifyContent: "center",
-                  alignItems: "center",
-                  cursor: "pointer",
+                sx={{
+                  padding: "8px",
+                  fontSize: "1.25rem",
+                  color: "var(--Project-Sandbox-White)",
                 }}
-              ></i>
+              >
+                <i className="fa-solid fa-bars"></i>
+              </IconButton>
             )}
-            <img src="../src/img/logo.svg" alt="Logo" />
-            <Typography
-              component="p"
-              sx={{
-                color: "var(--Project-Sandbox-White)",
-                fontSize: "18px",
-                fontWeight: 600,
-                marginLeft: 1,
+
+            <Box
+              onClick={() => {
+                handleScrollToTop();
+                navigate("/");
               }}
+              sx={{ cursor: "pointer", display: "flex", alignItems: "center" }}
             >
-              WDB
-            </Typography>
+              <img src="../src/img/logo.svg" alt="Logo" />
+              <Typography
+                component="p"
+                sx={{
+                  color: "var(--Project-Sandbox-White)",
+                  fontSize: "18px",
+                  fontWeight: 600,
+                  ml: 1,
+                }}
+              >
+                WDB
+              </Typography>
+            </Box>
 
             {!isSmallScreen && (
               <Box
@@ -104,108 +103,85 @@ const Header = ({ currentPermalink = null }) => {
               >
                 <Stack direction="row" spacing={2}>
                   {categoryTypes.map((catType) => (
-                    <div key={catType} style={{ position: "relative" }}>
+                    <Box key={catType} sx={{ position: "relative" }}>
                       <Button
+                        onClick={() => handleCategoryClick(catType)}
                         sx={{
                           color:
-                            catType === activeCategory
+                            activeCategory === catType
                               ? "var(--Project-Sandbox-Primary-Red-700)"
                               : "var(--Project-Sandbox-White)",
-                          ":hover": {
+                          "&:hover": {
                             color: "var(--Project-Sandbox-Primary-Red-700)",
                           },
-                        }}
-                        onClick={() => {
-                          if (catType === "Home") {
-                            handleScrollToTop();
-                            navigate("/");
-                          } else {
-                            handleCategoryClick(catType);
-                          }
                         }}
                       >
                         {catType}
                       </Button>
-                      {activeCategory !== "Home" && (
-                        <Fade in={activeCategory === catType}>
-                          <List
-                            sx={{
-                              position: "absolute",
-                              top: "100%",
-                              left: 0,
-                              backgroundColor: "white",
-                              boxShadow: 2,
-                              zIndex: 1,
-                              width: "200px",
-                              cursor: "pointer",
-                            }}
-                          >
-                            {categoryData
-                              .filter((cat) => cat.type === catType)
-                              .map((cat) => (
-                                <ListItem
-                                  key={cat.api}
-                                  button
-                                  onClick={() => handleSubItemClick(cat.api)}
-                                  sx={{
+                      <Fade in={activeCategory === catType}>
+                        <List
+                          sx={{
+                            position: "absolute",
+                            top: "100%",
+                            left: 0,
+                            backgroundColor: "white",
+                            boxShadow: 2,
+                            zIndex: 1,
+                            width: "200px",
+                          }}
+                        >
+                          {categoryData
+                            .filter((cat) => cat.type === catType)
+                            .map((cat) => (
+                              <ListItem
+                                key={cat.api}
+                                button
+                                onClick={() => handleSubItemClick(cat.api)}
+                                sx={{
+                                  backgroundColor:
+                                    cat.api === currentPermalink
+                                      ? "var(--Project-Sandbox-Primary-Red-700)"
+                                      : "inherit",
+                                  cursor:
+                                    cat.api === currentPermalink
+                                      ? "default"
+                                      : "pointer",
+                                  "&:hover": {
                                     backgroundColor:
                                       cat.api === currentPermalink
                                         ? "var(--Project-Sandbox-Primary-Red-700)"
-                                        : "inherit",
-                                    cursor:
-                                      cat.api === currentPermalink
-                                        ? "default"
-                                        : "pointer",
-                                    ":hover": {
-                                      backgroundColor:
-                                        cat.api === currentPermalink
-                                          ? "var(--Project-Sandbox-Primary-Red-700)"
-                                          : "var(--Project-Sandbox-Secondary-Black-300)",
-                                    },
+                                        : "var(--Project-Sandbox-Secondary-Black-300)",
+                                  },
+                                }}
+                              >
+                                <ListItemText
+                                  primary={cat.text}
+                                  sx={{
+                                    color:
+                                      "var(--Project-Sandbox-Secondary-Black-900)",
                                   }}
-                                >
-                                  <ListItemText
-                                    primary={cat.text}
-                                    sx={{
-                                      color:
-                                        "var(--Project-Sandbox-Secondary-Black-900)",
-                                    }}
-                                  />
-                                </ListItem>
-                              ))}
-                          </List>
-                        </Fade>
-                      )}
-                    </div>
+                                />
+                              </ListItem>
+                            ))}
+                        </List>
+                      </Fade>
+                    </Box>
                   ))}
                 </Stack>
               </Box>
             )}
 
-            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
-              <IconButton
-                onClick={() => navigate("/cart")}
-                color="inherit"
-                size="small"
-              >
+            <IconButton
+              onClick={() => navigate("/cart")}
+              sx={{ color: "inherit" }}
+            >
+              <Badge badgeContent={totalItemsInCart} color="error">
                 <i className="fa-solid fa-cart-shopping"></i>
-                <Badge
-                  badgeContent={totalItemsInCart}
-                  color="error"
-                  sx={{ position: "absolute", top: "-1px", right: "-1px" }}
-                />
-              </IconButton>
-            </Box>
+              </Badge>
+            </IconButton>
           </Toolbar>
         </Container>
       </AppBar>
-
-      {isSmallScreen && (
-        <SidebarSelector
-          open={sidebarOpen}
-          onClose={() => setSidebarOpen(false)}
-        />
-      )}
     </>
   );
 };
